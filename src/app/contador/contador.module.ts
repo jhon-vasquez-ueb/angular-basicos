297e389bf94d70139b75b2eb175e796fdf464e26
Jhonn Vasquez <jhonjuanhs@gmail.com>
2022-05-26 17:16:27 -0500

    Primer Commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import { ContadorComponent } from './contador/contador.component';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    declarations: [
+        ContadorComponent,
+    ],
+    exports: [
+        ContadorComponent,
+    ]
+})
+export class ContadorModule {}
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent  {
+
+  heroes: string [] = [ 'Spiderman', 'IronMan', 'Hulk', 'Thor', 'Capitán América'];  
+  heroeBorrado: string = '';
+
+  borrarHeroe ( ) {
+   this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+  
+
+}
